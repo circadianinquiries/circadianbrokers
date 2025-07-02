fd66308e6a57028e6ff43e5df8948ae60660a0bb
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-07-02 20:39:10 +0500

    chat bot integrated

--- src/app/GTM.jsx
+++ src/app/GTM.jsx
@@ -4,11 +4,11 @@ import Script from "next/script";
 
 const GTM_ID = "GTM-K2PDTJCJ";
 const GA_ID = "G-6Y11JM000C";
+const OLARK_ID = "9396-319-10-3792";
 
 export default function GTM() {
     return (
         <>
-
             {/* Google Tag Manager */}
             <Script id="gtm-init" strategy="afterInteractive">
                 {`
@@ -21,13 +21,33 @@ export default function GTM() {
             </Script>
 
             {/* Google Analytics */}
-            <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+                strategy="afterInteractive"
+            />
             <Script id="ga-init" strategy="afterInteractive">
                 {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${GA_ID}');
+        `}
+            </Script>
+
+            {/* Olark Live Chat */}
+            <Script id="olark-init" strategy="afterInteractive">
+                {`
+          ;(function(o,l,a,r,k,y){if(o.olark)return;
+            r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
+            y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
+            y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
+            y.extend=function(i,j){y("extend",i,j)};
+            y.identify=function(i){y("identify",k.i=i)};
+            y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
+            k=y._={s:[],t:[+new Date],c:{},l:a};
+          })(window,document,"static.olark.com/jsclient/loader.js");
+
+          olark.identify('${OLARK_ID}');
         `}
             </Script>
         </>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,7 @@ import TopBar from "@/src/app/layout/topbar";
 import { Toaster } from "react-hot-toast";
 import GTM from "@/src/app/GTM";
 
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800", "900"],
