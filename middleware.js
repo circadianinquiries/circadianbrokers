3693bb4f665e1b88e00d212a4fce023cb3e01934
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-12-04 17:20:11 +0500

    blog set and push

--- middleware.js
+++ middleware.js
@@ -1,21 +1,23 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export function middleware(req) {
-  const country = req.geo?.country || "UNKNOWN";
-  console.log("Vercel detected country:", country); // This will appear in Vercel logs
+// export function middleware(req) {
+//   const country = req.geo?.country || "UNKNOWN";
+//   console.log("Vercel detected country:", country); // This will appear in Vercel logs
 
-  const blockCountries = process.env.NEXT_PUBLIC_BLOCK_COUNTRIES === "true";
-  console.log("Block countries enabled:", blockCountries);
+//   const blockCountries = process.env.NEXT_PUBLIC_BLOCK_COUNTRIES === "true";
+//   console.log("Block countries enabled:", blockCountries);
 
-  if (!blockCountries) return NextResponse.next();
+//   if (!blockCountries) return NextResponse.next();
 
-  if (country !== "PK") {
-    return new NextResponse("Not available in your region", { status: 403 });
-  }
+//   if (country !== "PK") {
+//     return new NextResponse("Not available in your region", { status: 403 });
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/:path*"], // Make sure this matches all your pages
-};
+// export const config = {
+//   matcher: ["/:path*"], // Make sure this matches all your pages
+// };
+
+
