140e0e9e62103d07b1fa2c7a130e10d868df7e1e
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-12-04 19:05:32 +0500

    add Coputry

---
+++ src/app/blocked/layout.jsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: "",
+  description: "",
+  //===== OG Tags =====
+  openGraph: {
+    title: "",
+    description: "",
+    url: "/blocked",
+    siteName: " Circadian Insurance Brokers",
+    locale: "en_US",
+    type: "website",
+    images: "https://www.circadianbrokers.com/logo.webp",
+  },
+  //===== Canonical =====
+  alternates: { canonical: "/blocked" },
+
+};
+
+export default function RootLayout({ children }) {
+  return children;
+}
